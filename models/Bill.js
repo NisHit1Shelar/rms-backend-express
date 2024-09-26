a11dd7f117c0ec5b47554b37590997e1c3926010
NisHit1Shelar <nishit.shelar23@vit.edu>
2024-09-26 11:44:46 +0530

    Update Bill.js

--- models/Bill.js
+++ models/Bill.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 
-// Bill Schema
-const BillSchema = new mongoose.Schema({
+const billSchema = new mongoose.Schema({
     table: { type: String, required: true },
-    total: { type: Number, required: true },
     items: [
         {
             id: { type: Number, required: true },
             name: { type: String, required: true },
             price: { type: Number, required: true },
-            quantity: { type: Number, required: true },
-        },
+            quantity: { type: Number, required: true }
+        }
     ],
+    total: { type: Number, required: true },
+    createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Bill', BillSchema);
+const Bill = mongoose.model('Bill', billSchema);
+
+module.exports = Bill;
